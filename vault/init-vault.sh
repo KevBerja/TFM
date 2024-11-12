225e9625167a5f9e36f27527f3e7f04755afb79d
Kevin Carracedo Vázquez <kevirgitano@gmail.com>
2024-11-12 18:43:00 +0100

    Configurando Keycloak y Vault modo servidor en Produccion, Conexóon con postgres y volúmenes funcionando, Se extrae root_token de Vault y claves seal y unseal

---
+++ vault/init-vault.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+export VAULT_ADDR='http://vault:8200'
+
+# Esperar a que Vault esté disponible
+until curl -s $VAULT_ADDR/v1/sys/health > /dev/null; do
+  echo "Esperando a que Vault esté disponible..."
+  sleep 1
+done
+
+# Verificar si Vault está inicializado
+if [ "$(vault status -format=json | jq -r '.initialized')" = "false" ]; then
+  echo "Inicializando Vault..."
+  vault operator init -key-shares=1 -key-threshold=1 -format=json > /vault/data/keys.json
+
+  UNSEAL_KEY=$(jq -r '.unseal_keys_b64[0]' /vault/data/keys.json)
+  ROOT_TOKEN=$(jq -r '.root_token' /vault/data/keys.json)
+
+  # Des-sellar Vault
+  vault operator unseal $UNSEAL_KEY
+
+  echo "Vault inicializado y des-sellado."
+else
+  echo "Vault ya está inicializado. Des-sellando..."
+  UNSEAL_KEY=$(jq -r '.unseal_keys_b64[0]' /vault/data/keys.json)
+  vault operator unseal $UNSEAL_KEY
+  echo "Vault des-sellado."
+fi
